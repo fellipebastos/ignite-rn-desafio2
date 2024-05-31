d48e70df3b6d501594a603f66ca303a9179103b4
fellipebastos <fellipebastoswm@gmail.com>
2024-05-31 16:43:44 -0300

    feat: add PageContainer

---
+++ src/components/PageContainer/index.tsx
@@ -0,0 +1,7 @@
+import { ViewProps } from 'react-native'
+
+import { PageContainerStyle } from './styles'
+
+export function PageContainer(props: ViewProps) {
+  return <PageContainerStyle {...props} />
+}
