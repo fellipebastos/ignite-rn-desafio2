cb75f33e1ca979fc152bcf0329bc3f3d1e51cbef
fellipebastos <fellipebastoswm@gmail.com>
2024-05-31 14:51:32 -0300

    feat: add Nunito-Sans font

--- App.tsx
+++ App.tsx
@@ -4,8 +4,22 @@ import { ThemeProvider } from 'styled-components/native'
 import { Routes } from '@routes/index'
 
 import theme from './src/theme'
+import {
+  NunitoSans_400Regular,
+  NunitoSans_700Bold,
+  useFonts,
+} from '@expo-google-fonts/nunito-sans'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    NunitoSans_400Regular,
+    NunitoSans_700Bold,
+  })
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <StatusBar
