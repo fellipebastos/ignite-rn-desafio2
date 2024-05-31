f43e56e49d4643288cc78eb1184125a363fac289
fellipebastos <fellipebastoswm@gmail.com>
2024-05-31 20:36:33 -0300

    refactor: remove Container component form Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,3 @@
-import { Container } from '@components/Container'
-
 import logoImg from '@assets/logo.png'
 import avatarImg from '@assets/avatar.png'
 
@@ -7,11 +5,9 @@ import { Avatar, HeaderContainer, Logo } from './styles'
 
 export function Header() {
   return (
-    <Container>
-      <HeaderContainer>
-        <Logo source={logoImg} alt="Logo" />
-        <Avatar source={avatarImg} alt="Avatar" />
-      </HeaderContainer>
-    </Container>
+    <HeaderContainer>
+      <Logo source={logoImg} alt="Logo" />
+      <Avatar source={avatarImg} alt="Avatar" />
+    </HeaderContainer>
   )
 }
